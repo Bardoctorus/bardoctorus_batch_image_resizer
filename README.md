1daf6f1d3e433969fca03a5512172b08e360e3ee
Ian Buckley <ian.m.buckley@gmail.com>
2019-05-12 23:55:23 +0200

    Initial commit innit

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-# batch_image_sizer
-A simple script to change the size of all images in a folder while maintaining their aspect ratio.
+# bardoctorus_batch_image_sizer
+A command line tool which converts every image in a chosen folder to a new size using Lanczos, and saves it with a prefix.
 
-## Cheese.
+# how_to_get_it_working
+
+Use Python 3.6.7, because it's untested on anything that isn't my Linux laptop and Python 3.6.7. Python 2.7? 
+
+Nope. Y?
+
+Requires <a hred="https://pillow.readthedocs.io/en/stable/index.html">Pillow</a> to work 
+
+So far this is WYSIWYG, I take no responsibility if it deletes your bank or eats your cat.
+
+# how_to_use_it
+For a simple example, clone the ting, install pillow, run it in the terminal, follow the on screen instructions. I've included a cat to practice on.
+
+https://github.com/Bardoctorus/batch_image_sizer/blob/master/ket.png
+
--- image_resize.py
+++ image_resize.py
@@ -25,40 +25,81 @@ def main(folder, newWidth):
     
 
     onlyfiles = [f for f in listdir(folder) if isfile(join(folder, f))]
-    print(onlyfiles)
-
+    #print(type(onlyfiles[0]))
+    succesful_conversions = []
     for file in onlyfiles:
-        file_checker(file,newWidth)
+        joined_name = os.path.join(folder, file)
+        print("Attempting to open "+joined_name)
+        try:
+            f =  Image.open(joined_name)
 
-
-def file_checker(file, newWidth):
-    try:
-        f = Image.open(file)
-        if (f.size > newWidth):
-            makeBigga(f)
-        elif (f.size < newWidth):
-            makeSmol(f)
+        except IOError:
+            print("Error opening "+file+", moving on")
+            continue
         else:
-            print("Image already correct width - moving on")
-            return
-    except:
-        print("Couldn't open file: "+file+" - moving on")    
-        return
+            
+            newHeight =  int(newWidth * f.size[1]/f.size[0])
+            newSize = (newWidth, newHeight)
+            newFileName = os.path.join(folder,  "_"+str(newWidth)+"px_"+file)
+            # deets = {"new height": newHeight,
+            #      "new Size": newSize,
+            #      "new file name": newFileName  }
+            # print(deets)  
+            g = resizer(f, newSize)
+            g.save(newFileName)
+            succesful_conversions.append(newFileName)
+            #print("Created: "+newFileName)
+    print(".\n.\n.\n.\nDone! Created:\n")
+    for thing in succesful_conversions:
+        print (thing)
+
     
+    
+def resizer(imData, newSize):
+    # ! all this if logic is only needed if you are using a different filter for up/downscaling
+      # if (f.size[0] > newSize[0]):
+          
+        #     g = makeSmol(f, newSize)
+            
+        #     g.save(newName)
+        # elif (f.size[0] < newWidth):
+        #     g = makeBigga(f, newSize)
+            
+        #     g.save(newName)
+        # else:
+        #     print("Image already correct width - moving on")
+        #     return
+    g = imData.resize((newSize),Image.LANCZOS)
+    return g
 
 
-def makeBigga(file):
-    print("this is big time")
-
-def makeSmol(file):
-    print("squashy squashy")    
-
-def saveTing():
-    print("super saver")
     
 if __name__ == "__main__":
-    path = input("Enter folder path, or leave blank to use current folder.\n.")
+    print(".______   .______    __  .______     ") 
+    print("|   _  \  |   _  \  |  | |   _  \     ")
+    print("|  |_)  | |  |_)  | |  | |  |_)  |    ")
+    print("|   _  <  |   _  <  |  | |      /     ")
+    print("|  |_)  | |  |_)  | |  | |  |\  \-.")
+    print("|______/  |______/  |__| | _| `.__| Bardoctorus Batch Image Resizer\n")
+    print("This simple image resizer detects all the images in a folder and resizes them to a chosen pixel width using Lanczos, while preserving the aspect ratio.")
+    print("\n\nNo files are overwritten, new files get the prefix '_NEWSIZEpx_'")
+    
+    print("Press Ctrl + C to quit\n\n")
+    path = input("Enter folder path, or press Enter to use current folder, or info for more info\n.")
     if path == "":
         path = os.getcwd()
+    elif path == "info":
+        print("\nFull info available at https://github.com/Bardoctorus/UPDATETHISURL but in short:\n")
+        print("This script iterates over every file in a directory using Pillow's Image.open function.")
+        print("I'm pretty much just letting it do its thing in a try, and excepting any IOErrors before continuing.")
+        print("New file aspect ratios are just 'new_width * old_height / old_width' casted to an int which I'm sure makes some of you cry.")
+        print("Feel free to fork and improve this program and tell me how much I suck - @ianmbuckley on the twitderp\n\n")
+        path = input("Enter folder path, or press Enter to use current folder.\n.")
+        if path == "":
+            path = os.getcwd()
     size = input("Enter new width.\n.") 
     main(path, size)
+
+
+
+                                      
